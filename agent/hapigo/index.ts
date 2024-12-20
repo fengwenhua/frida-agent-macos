c1a35e3ddf80ed53c100765dbf7eef82db150a4f
fengwenhua02 <fengwenhua02@qianxin.com>
2024-12-20 17:26:02 +0800

    feat: update hapigo to 2.15.0

--- agent/hapigo/index.ts
+++ agent/hapigo/index.ts
@@ -2,7 +2,7 @@ import { HookApp, log } from './agent/Utils.js';
 
 HookApp('HapiGo', (hook, getPointer, getClassMethod, appBaseAddr) => {
   hook(
-    getPointer('0x2F2B40'),
+    getPointer('0x302630'),
     (ths, retv) => {
       retv.replace(ptr(1));
     },
